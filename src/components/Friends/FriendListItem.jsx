518658e473fe1750f5011086ae485abafdf2f80b
Timofey_Slakva <111429897+Lefirmor@users.noreply.github.com>
2023-09-20 22:15:08 +0200

    comments

--- src/components/Friends/FriendListItem.jsx
+++ src/components/Friends/FriendListItem.jsx
@@ -1,4 +1,4 @@
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import css from './Friends.module.css';
 
 export const FriendListItem = ({ avatar, name, isOnline }) => {
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,4 +1,4 @@
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import css from './Statistics.module.css';
 export const StatisticsData = ({ title, stats }) => {
   return (
